82830eed9b60b84248f6a1cfe25d2da3af8fce79
HickupHH3 <hickuphh3@gmail.com>
2023-11-09 11:27:48 +0800

    Add start time, increase num mints & rate

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -14,7 +14,11 @@ contract DeployScript is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        X_IST XIST = new X_IST(1700006399);
+        // for actual game:
+        // startTime = 13th Nov 9am TRT = 13th Nov 6am UTC = 1699855200
+        // endTime = 14th Nov 12pm TRT = 14th Nov 9am UTC = 1699952400
+        X_IST XIST = new X_IST(block.timestamp, 1699952400);
+        // X_IST XIST = new X_IST(1699855200, 1699952400);
         console.log("XIST addr:", address(XIST));
         PixelsMap pixelsMap;
         // use create2 just in case someone manages to destroy the contract
--- contracts/src/X_IST.sol
+++ contracts/src/X_IST.sol
@@ -4,19 +4,21 @@ pragma solidity ^0.8.19;
 import {ERC20} from "oz/token/ERC20/ERC20.sol";
 
 contract X_IST is ERC20 {
-    uint256 public constant MAX_TOTAL_MINTS = 2023;
-    uint256 public constant MAX_MINTS_PER_EPOCH = 12;
+    uint256 public constant MAX_TOTAL_MINTS = 3202;
+    uint256 public constant MAX_MINTS_PER_EPOCH = 15;
     uint256 internal constant EPOCH_PERIOD = 25; // each epoch = 25 blocks (5 mins)
     uint256 internal constant MIN_MINT_AMT = 400e18;
     uint256 internal constant MAX_MINT_AMT = 600e18;
     uint256 public numMints;
+    uint256 internal gameStartTime;
     uint256 public gameEndTime;
     address public owner;
     address public pixelsMap;
     mapping (uint256 => uint256) mintsInEpoch;
 
-    constructor(uint256 _gameEndTime) ERC20("TrustX-Istanbul", "X_IST") {
+    constructor(uint256 _gameStartTime, uint256 _gameEndTime) ERC20("TrustX-Istanbul", "X_IST") {
         owner = msg.sender;
+        gameStartTime = _gameStartTime;
         gameEndTime = _gameEndTime;
     }
 
@@ -32,15 +34,16 @@ contract X_IST is ERC20 {
 
     /// @dev mint pseudorandom amount 
     /// has a mint cap and is rate limited, will NOT mint if either limit is exceeded
-    /// also mints equivalent amount to pixelMap
+    /// also mints 2x amount to pixelMap
     function mint(address to) external {
         require(msg.sender == pixelsMap, "unauthorized");
+        require(block.timestamp < gameStartTime, "too early");
         if (++numMints > MAX_TOTAL_MINTS || ++mintsInEpoch[block.number / EPOCH_PERIOD] > MAX_MINTS_PER_EPOCH) return;
         // calculate random amount to mint
         uint256 seed = uint256(keccak256(abi.encodePacked(to, block.number)));
         uint256 mintAmt = seed % (MAX_MINT_AMT - MIN_MINT_AMT) + MIN_MINT_AMT;
         _mint(to, mintAmt);
-        _mint(msg.sender, mintAmt);
+        _mint(msg.sender, mintAmt * 2);
     }
 
     function mint(address to, uint256 amt) external {
